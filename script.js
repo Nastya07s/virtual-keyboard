cc08712206f7158925459f0948576fbd71f19922
Nastya07s <nastya.kotovich07@gmail.com>
2020-04-04 12:02:23 +0300

    feat: add link to physical keyboard

--- script.js
+++ script.js
@@ -38,6 +38,116 @@ class Keyboard {
     this.elements.main.appendChild(this.elements.keyboard);
 
     document.body.appendChild(this.elements.main);
+
+    window.addEventListener('keydown', event => {
+      console.log(event);
+      let key = event.key;
+      // if (event.key !== 'CapsLock')
+      console.log(key);
+      console.log(this.elements.keys);
+      Array.from(this.elements.keys)
+        .filter(el => el.textContent === key || el.textContent.length === 0 && key === ' ' || el.textContent === 'Ctrl' && key === 'Control' || el.textContent === 'Win' && key === 'Meta')
+        .forEach(el => this._togglePress(el, true));
+      switch (key) {
+        case 'Backspace':
+          this.elements.textarea.focus();
+          event.preventDefault();
+          if (this.elements.textarea.selectionStart !== 0) {
+            if (this.elements.textarea.selectionEnd === this.elements.textarea.selectionStart) {
+              this.properties.valueInput = this.properties.valueInput.substring(0, this.elements.textarea.selectionStart - 1) + this.properties.valueInput.substring(this.elements.textarea.selectionStart, this.properties.valueInput.length);
+              this._oninput(key, this.elements.textarea.selectionStart - 1);
+            } else {
+              this.properties.valueInput = this.properties.valueInput.substring(0, this.elements.textarea.selectionStart) + this.properties.valueInput.substring(this.elements.textarea.selectionEnd, this.properties.valueInput.length);
+              this._oninput(key, this.elements.textarea.selectionStart);
+            }
+          } else
+            this._oninput(key, this.elements.textarea.selectionStart);
+
+          break;
+
+        case 'Delete':
+          this.elements.textarea.focus();
+          event.preventDefault();
+          if (this.elements.textarea.selectionEnd === this.elements.textarea.selectionStart)
+            this.properties.valueInput = this.properties.valueInput.substring(0, this.elements.textarea.selectionStart) + this.properties.valueInput.substring(this.elements.textarea.selectionStart + 1, this.properties.valueInput.length);
+          else
+            this.properties.valueInput = this.properties.valueInput.substring(0, this.elements.textarea.selectionStart) + this.properties.valueInput.substring(this.elements.textarea.selectionEnd, this.properties.valueInput.length);
+          this._oninput(key, this.elements.textarea.selectionStart);
+
+          break;
+
+        case "Tab":
+          this.elements.textarea.focus();
+          event.preventDefault();
+          this.properties.valueInput = this.properties.valueInput.substring(0, this.elements.textarea.selectionStart) + '\t' + this.properties.valueInput.substring(this.elements.textarea.selectionStart, this.properties.valueInput.length);
+          this._oninput(key, ++this.elements.textarea.selectionStart);
+          break;
+
+        case "CapsLock":
+          this._toggleCapsLock();
+          Array.from(this.elements.keys).find(el => el.textContent === event.key).classList.toggle('keyboard-key-pressed', this.properties.capsLock);
+          break;
+
+        case "Enter":
+          this.elements.textarea.focus();
+          event.preventDefault();
+          this.properties.valueInput = this.properties.valueInput.substring(0, this.elements.textarea.selectionStart) + '\n' + this.properties.valueInput.substring(this.elements.textarea.selectionStart, this.properties.valueInput.length);
+          this._oninput(key, ++this.elements.textarea.selectionStart);
+
+          break;
+
+        case " ":
+          this.elements.textarea.focus();
+          event.preventDefault();
+          this.properties.valueInput = this.properties.valueInput.substring(0, this.elements.textarea.selectionStart) + ' ' + this.properties.valueInput.substring(this.elements.textarea.selectionStart, this.properties.valueInput.length);
+          this._oninput(key, ++this.elements.textarea.selectionStart);
+
+          break;
+
+        case "Shift":
+          break;
+
+        case "Home":
+          this.elements.textarea.focus();
+          event.preventDefault();
+          this._oninput(key, 0);
+          break;
+
+        case "End":
+          this.elements.textarea.focus();
+          event.preventDefault();
+          this._oninput(key, this.properties.valueInput.length);
+          break;
+
+        case "Control":
+          break;
+
+        case "Meta":
+          // event.preventDefault();
+          break;
+
+        case "Alt":
+          break;
+
+
+        default:
+          this.elements.textarea.focus();
+          event.preventDefault();
+          let keySymbol = this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+          this.properties.valueInput = this.properties.valueInput.substring(0, this.elements.textarea.selectionStart) + keySymbol + this.properties.valueInput.substring(this.elements.textarea.selectionStart, this.properties.valueInput.length);
+          this._oninput(key, ++this.elements.textarea.selectionStart);
+
+          break;
+      }
+    });
+
+    window.addEventListener('keyup', event => {
+      console.log(event);
+      if (event.key !== 'CapsLock')
+        Array.from(this.elements.keys)
+        .filter(el => el.textContent === event.key || el.textContent.length === 0 && event.key === ' ' || el.textContent === 'Ctrl' && event.key === 'Control' || el.textContent === 'Win' && event.key === 'Meta')
+        .forEach(el => this._togglePress(el, false));
+    })
   }
 
   _createKeys() {
@@ -47,13 +157,13 @@ class Keyboard {
       'tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'delete',
       'capslock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'enter',
       'shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'rshift',
-      'ctrl', 'win', 'alt', 'whitespace', 'ctrl', 'home', '←', '↓', '→', 'end',
+      'ctrl', 'win', 'alt', 'space', 'ctrl', 'home', '←', '↓', '→', 'end',
     ] : [
       '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
       'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'delete',
       'capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'enter',
       'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'rshift',
-      'ctrl', 'win', 'alt', 'whitespace', 'ctrl', 'home', '←', '↓', '→', 'end',
+      'ctrl', 'win', 'alt', 'space', 'ctrl', 'home', '←', '↓', '→', 'end',
     ];
 
     keysLayout.forEach(key => {
@@ -134,7 +244,7 @@ class Keyboard {
           });
           break;
 
-        case "whitespace":
+        case "space":
           keyElement.classList.add('keyboard-key-extra-wide');
 
           keyElement.addEventListener('click', () => {
@@ -228,6 +338,12 @@ class Keyboard {
       if (key.textContent.length === 1)
         key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
     }
+  }
+
+  _togglePress(element, option) {
+
+    element.classList.toggle('keyboard-key-pressed', option);
+    console.log(element);
   }
 }
 
